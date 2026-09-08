ff742e92760b64ada1d39bd3483d434b050a7b64
Savio Lima <saviiolima@gmail.com>
2026-09-08 14:16:01 -0300

    feat: adiciona rastreamento com vercel analytics

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { GlobalStyle } from "./Styles/globalstyle";
-import Routes from "./Routes/routes";
+import App from "./Routes/routes.jsx";
+import { Analytics } from "@vercel/analytics/react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <GlobalStyle />
-    <Routes />
-  </React.StrictMode>
+    <App />
+    <Analytics />
+  </React.StrictMode>,
 );
